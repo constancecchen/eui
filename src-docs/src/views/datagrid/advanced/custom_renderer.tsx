10f10ac77ebfa383b60abac2f950159e74254ed2
Cee Chen <constance.chen@elastic.co>
2023-10-19 18:08:26 -0700

    wip

--- src-docs/src/views/datagrid/advanced/custom_renderer.tsx
+++ src-docs/src/views/datagrid/advanced/custom_renderer.tsx
@@ -1,18 +1,25 @@
-import React, { useEffect, useCallback, useState, useRef } from 'react';
+import React, {
+  useEffect,
+  useCallback,
+  useState,
+  useContext,
+  createContext,
+} from 'react';
 import { css } from '@emotion/react';
 import { faker } from '@faker-js/faker';
 
 import {
   EuiDataGrid,
   EuiDataGridProps,
-  EuiDataGridCustomBodyProps,
   EuiDataGridColumnCellActionProps,
+  EuiDataGridCellProps,
   EuiScreenReaderOnly,
-  EuiCheckbox,
   EuiButtonIcon,
-  EuiIcon,
+  EuiButton,
+  EuiMarkdownEditor,
+  EuiCommentList,
+  EuiConfirmModal,
   EuiFlexGroup,
-  EuiSwitch,
   EuiSpacer,
   useEuiTheme,
   logicalCSS,
@@ -67,100 +74,248 @@ const columns = [
   },
 ];
 
+type NotesMap = Record<number, { notes?: string[]; isAddingNote?: boolean }>;
+const NotesContext = createContext<{
+  notesMap: NotesMap;
+  setNotesMap: Function;
+}>({
+  notesMap: {},
+  setNotesMap: () => {},
+});
+
+const AddNoteAction: EuiDataGridCellProps['renderCellValue'] = ({
+  rowIndex,
+}) => {
+  const { setNotesMap } = useContext(NotesContext);
+
+  return (
+    <EuiButtonIcon
+      iconType="documentEdit"
+      aria-label="Add note to row"
+      title="Add note to row"
+      onClick={() =>
+        setNotesMap((notesMap: NotesMap) => {
+          const row = notesMap[rowIndex];
+          if (row?.isAddingNote) return notesMap; // If we're already adding a note, no need to update
+
+          return {
+            ...notesMap,
+            [rowIndex]: { ...row, isAddingNote: true },
+          };
+        })
+      }
+    />
+  );
+};
+
 const leadingControlColumns: EuiDataGridProps['leadingControlColumns'] = [
   {
     id: 'selection',
-    width: 32,
-    headerCellRender: () => (
-      <EuiCheckbox
-        id="select-all-rows"
-        aria-label="Select all rows"
-        onChange={() => {}}
-      />
-    ),
-    rowCellRender: ({ rowIndex }) => (
-      <EuiCheckbox
-        id={`select-row-${rowIndex}`}
-        aria-label="Select row"
-        onChange={() => {}}
-      />
-    ),
-  },
-];
-
-const trailingControlColumns: EuiDataGridProps['trailingControlColumns'] = [
-  {
-    id: 'actions',
     width: 40,
     headerCellRender: () => (
       <EuiScreenReaderOnly>
-        <span>Actions</span>
+        <span>Add note</span>
       </EuiScreenReaderOnly>
     ),
-    rowCellRender: () => (
-      <EuiButtonIcon iconType="boxesHorizontal" aria-label="See row actions" />
-    ),
+    rowCellRender: AddNoteAction,
   },
 ];
 
-// The custom row details is actually a trailing control column cell with
+const Notes: EuiDataGridCellProps['renderCellValue'] = ({
+  rowIndex,
+  setCellProps,
+}) => {
+  const { notesMap, setNotesMap } = useContext(NotesContext);
+  const row = notesMap[rowIndex];
+
+  // When rendering this custom cell, we'll want to override
+  // the automatic width/heights calculated by EuiDataGrid
+  useEffect(() => {
+    setCellProps({ style: { width: '100%', height: 'auto' } });
+  }, [row, setCellProps]);
+
+  const [editingValue, setEditingValue] = useState('');
+  const [deleteConfirmation, showDeleteConfirmation] = useState<false | number>(
+    false
+  );
+
+  if (row?.isAddingNote) {
+    return (
+      <>
+        <EuiMarkdownEditor
+          aria-label="Add note"
+          placeholder="Add note"
+          value={editingValue}
+          onChange={setEditingValue}
+          initialViewMode="editing"
+          markdownFormatProps={{ textSize: 's' }}
+        />
+        <EuiSpacer size="s" />
+        <EuiFlexGroup
+          gutterSize="s"
+          justifyContent="spaceBetween"
+          responsive={false}
+        >
+          <EuiButton
+            onClick={() =>
+              setNotesMap((notesMap: NotesMap) => ({
+                ...notesMap,
+                [rowIndex]: {
+                  ...row,
+                  isAddingNote: false,
+                },
+              }))
+            }
+          >
+            Cancel
+          </EuiButton>
+          <EuiButton
+            fill
+            type="submit"
+            onClick={() =>
+              setNotesMap((notesMap: NotesMap) => ({
+                ...notesMap,
+                [rowIndex]: {
+                  notes: [...(row?.notes || []), editingValue],
+                  isAddingNote: false,
+                },
+              }))
+            }
+            isDisabled={!editingValue}
+          >
+            Save
+          </EuiButton>
+        </EuiFlexGroup>
+      </>
+    );
+  } else if (row?.notes?.length) {
+    const notes = row.notes.map((note, index) => ({
+      username: 'todo',
+      event: 'added a note',
+      children: <p>{note}</p>,
+      actions: [
+        <EuiButtonIcon
+          color="danger"
+          iconType="trash"
+          aria-label="Delete note"
+          title="Delete note"
+          onClick={() => showDeleteConfirmation(index)}
+        />,
+      ],
+    }));
+    return (
+      <>
+        <EuiCommentList comments={notes} />
+
+        {deleteConfirmation !== false && (
+          <EuiConfirmModal
+            title="Delete note?"
+            onCancel={() => showDeleteConfirmation(false)}
+            onConfirm={() => {
+              setNotesMap((notesMap: NotesMap) => {
+                return {
+                  ...notesMap,
+                  [rowIndex]: {
+                    notes: row.notes?.toSpliced(deleteConfirmation, 1),
+                    isAddingNote: false,
+                  },
+                };
+              });
+              showDeleteConfirmation(false);
+            }}
+            // This is bad copy
+            cancelButtonText="No, keep note"
+            confirmButtonText="Yes, delete note"
+            buttonColor="danger"
+            defaultFocusedButton="confirm"
+          />
+        )}
+      </>
+    );
+  }
+};
+
+// The custom add notes row is actually a trailing control column cell with
 // a hidden header. This is important for accessibility and markup reasons
 // @see https://fuschia-stretch.glitch.me/ for more
-const rowDetails: EuiDataGridProps['trailingControlColumns'] = [
+const trailingControlColumns: EuiDataGridProps['trailingControlColumns'] = [
   {
-    id: 'row-details',
+    id: 'row-notes',
 
     // The header cell should be visually hidden, but available to screen readers
     width: 0,
-    headerCellRender: () => <>Row details</>,
+    headerCellRender: () => <>Row notes</>,
     headerCellProps: { className: 'euiScreenReaderOnly' },
 
     // The footer cell can be hidden to both visual & SR users, as it does not contain meaningful information
     footerCellProps: { style: { display: 'none' } },
 
-    // When rendering this custom cell, we'll want to override
-    // the automatic width/heights calculated by EuiDataGrid
-    rowCellRender: ({ setCellProps, rowIndex }) => {
-      setCellProps({ style: { width: '100%', height: 'auto' } });
-
-      const firstName = raw_data[rowIndex].name.split(', ')[1];
-      const isGood = faker.datatype.boolean();
-      return (
-        <>
-          {firstName}&apos;s account has {isGood ? 'no' : ''} outstanding fees.{' '}
-          <EuiIcon
-            type={isGood ? 'checkInCircleFilled' : 'error'}
-            color={isGood ? 'success' : 'danger'}
-          />
-        </>
-      );
-    },
+    rowCellRender: Notes,
   },
 ];
 
-const footerCellValues: { [key: string]: string } = {
-  amount: `Total: ${raw_data
-    .reduce((acc, { amount }) => acc + Number(amount.split('$')[1]), 0)
-    .toLocaleString('en-US', { style: 'currency', currency: 'USD' })}`,
-};
-
-const RenderFooterCellValue: EuiDataGridProps['renderFooterCellValue'] = ({
-  columnId,
-  setCellProps,
+// Custom grid body renderer
+const RenderCustomGridBody: EuiDataGridProps['renderCustomGridBody'] = ({
+  Cell,
+  visibleColumns,
+  visibleRowData,
 }) => {
-  const value = footerCellValues[columnId];
+  // Ensure we're displaying correctly-paginated rows
+  const visibleRows = raw_data.slice(
+    visibleRowData.startRow,
+    visibleRowData.endRow
+  );
 
-  useEffect(() => {
-    // Turn off the cell expansion button if the footer cell is empty
-    if (!value) setCellProps({ isExpandable: false });
-  }, [value, setCellProps, columnId]);
+  // Add styling needed for custom grid body rows
+  const { euiTheme } = useEuiTheme();
+  const styles = {
+    row: css`
+      ${logicalCSS('width', 'fit-content')};
+      ${logicalCSS('border-bottom', euiTheme.border.thin)};
+      background-color: ${euiTheme.colors.emptyShade};
+    `,
+    rowCellsWrapper: css`
+      display: flex;
+    `,
+  };
 
-  return value || null;
+  const { notesMap } = useContext(NotesContext);
+
+  return (
+    <>
+      {visibleRows.map((row, rowIndex) => (
+        <div role="row" css={styles.row} key={rowIndex}>
+          <div css={styles.rowCellsWrapper}>
+            {visibleColumns.map((column, colIndex) => {
+              // Skip the row notes cell - we'll render it manually outside of the flex wrapper
+              if (column.id !== 'row-notes') {
+                return (
+                  <Cell
+                    colIndex={colIndex}
+                    visibleRowIndex={rowIndex}
+                    key={`${rowIndex},${colIndex}`}
+                  />
+                );
+              }
+            })}
+          </div>
+          {(notesMap[rowIndex]?.isAddingNote ||
+            (notesMap[rowIndex]?.notes?.length || 0) > 0) && (
+            <Cell
+              colIndex={visibleColumns.length - 1} // If the row is being shown, it should always be the last index
+              visibleRowIndex={rowIndex}
+            />
+          )}
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default () => {
-  const [autoHeight, setAutoHeight] = useState(true);
-  const [showRowDetails, setShowRowDetails] = useState(false);
+  const [notesMap, setNotesMap] = useState<NotesMap>({
+    0: { notes: ['Example note 1', 'Example note 2'] },
+  });
 
   // Column visibility
   const [visibleColumns, setVisibleColumns] = useState(() =>
@@ -189,106 +344,13 @@ export default () => {
     setSortingColumns(sortingColumns);
   }, []);
 
-  const { euiTheme } = useEuiTheme();
-
-  // Custom grid body renderer
-  const RenderCustomGridBody = useCallback(
-    ({
-      Cell,
-      visibleColumns,
-      visibleRowData,
-      setCustomGridBodyProps,
-    }: EuiDataGridCustomBodyProps) => {
-      // Ensure we're displaying correctly-paginated rows
-      const visibleRows = raw_data.slice(
-        visibleRowData.startRow,
-        visibleRowData.endRow
-      );
-
-      // Add styling needed for custom grid body rows
-      const styles = {
-        row: css`
-          ${logicalCSS('width', 'fit-content')};
-          ${logicalCSS('border-bottom', euiTheme.border.thin)};
-          background-color: ${euiTheme.colors.emptyShade};
-        `,
-        rowCellsWrapper: css`
-          display: flex;
-        `,
-        rowDetailsWrapper: css`
-          text-align: center;
-          background-color: ${euiTheme.colors.body};
-        `,
-      };
-
-      // Set custom props onto the grid body wrapper
-      const bodyRef = useRef<HTMLDivElement | null>(null);
-      useEffect(() => {
-        setCustomGridBodyProps({
-          ref: bodyRef,
-          onScroll: () =>
-            console.debug('scrollTop:', bodyRef.current?.scrollTop),
-        });
-      }, [setCustomGridBodyProps]);
-
-      return (
-        <>
-          {visibleRows.map((row, rowIndex) => (
-            <div role="row" css={styles.row} key={rowIndex}>
-              <div css={styles.rowCellsWrapper}>
-                {visibleColumns.map((column, colIndex) => {
-                  // Skip the row details cell - we'll render it manually outside of the flex wrapper
-                  if (column.id !== 'row-details') {
-                    return (
-                      <Cell
-                        colIndex={colIndex}
-                        visibleRowIndex={rowIndex}
-                        key={`${rowIndex},${colIndex}`}
-                      />
-                    );
-                  }
-                })}
-              </div>
-              {showRowDetails && (
-                <div css={styles.rowDetailsWrapper}>
-                  <Cell
-                    colIndex={visibleColumns.length - 1} // If the row is being shown, it should always be the last index
-                    visibleRowIndex={rowIndex}
-                  />
-                </div>
-              )}
-            </div>
-          ))}
-        </>
-      );
-    },
-    [showRowDetails, euiTheme]
-  );
-
   return (
-    <>
-      <EuiFlexGroup alignItems="center">
-        <EuiSwitch
-          label="Set static grid height"
-          checked={!autoHeight}
-          onChange={() => setAutoHeight(!autoHeight)}
-        />
-        <EuiSwitch
-          label="Toggle custom row details"
-          checked={showRowDetails}
-          onChange={() => setShowRowDetails(!showRowDetails)}
-        />
-      </EuiFlexGroup>
-      <EuiSpacer />
+    <NotesContext.Provider value={{ notesMap, setNotesMap }}>
       <EuiDataGrid
         aria-label="Data grid custom body renderer demo"
         columns={columns}
         leadingControlColumns={leadingControlColumns}
-        trailingControlColumns={
-          showRowDetails
-            ? [...trailingControlColumns, ...rowDetails]
-            : trailingControlColumns
-        }
+        trailingControlColumns={trailingControlColumns}
         columnVisibility={{ visibleColumns, setVisibleColumns }}
         sorting={{ columns: sortingColumns, onSort }}
         inMemory={{ level: 'sorting' }}
@@ -301,11 +363,9 @@ export default () => {
         renderCellValue={({ rowIndex, columnId }) =>
           raw_data[rowIndex][columnId]
         }
-        renderFooterCellValue={RenderFooterCellValue}
         renderCustomGridBody={RenderCustomGridBody}
-        height={autoHeight ? undefined : 400}
         gridStyle={{ border: 'none', header: 'underline' }}
       />
-    </>
+    </NotesContext.Provider>
   );
 };
